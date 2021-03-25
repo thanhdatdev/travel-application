ea5e54ec46bde40253609789a039f583a0df007e
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-03-26 02:50:36 +0700

    handle-signin/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#show"
+
+  as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
